d61ceb7fc45064b79b12bcb0d2e33431275aa97f
gundogduonur <gundogduonur@outlook.com.tr>
2022-06-12 22:13:57 +0300

    using List with Foreach

---
+++ SwiftUIBootCamp/SwiftUIBootCamp/ListUI.swift
@@ -0,0 +1,26 @@
+//
+//  ListUI.swift
+//  SwiftUIBootCamp
+//
+//  Created by Onur Gündoğdu on 12.06.2022.
+//
+
+import SwiftUI
+
+struct ListUI: View {
+    let myArray = ["Swift","SwiftUI","Apple","IOS"]
+    var body: some View {
+        List
+        {
+            ForEach(myArray, id: \.self) { value in
+                Text(value)
+            }
+        }
+    }
+}
+
+struct ListUI_Previews: PreviewProvider {
+    static var previews: some View {
+        ListUI()
+    }
+}
