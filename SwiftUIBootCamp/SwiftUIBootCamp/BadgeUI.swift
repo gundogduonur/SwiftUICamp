64f97230cc2ca3d3be8773c94045495e495b946a
gundogduonur <gundogduonur@outlook.com.tr>
2022-07-05 23:12:43 +0300

    Badge feature was used by assigning a random number

---
+++ SwiftUIBootCamp/SwiftUIBootCamp/BadgeUI.swift
@@ -0,0 +1,29 @@
+//
+//  BadgeUI.swift
+//  SwiftUIBootCamp
+//
+//  Created by Onur Gündoğdu on 5.07.2022.
+//
+
+import SwiftUI
+
+struct BadgeUI: View {
+    let randomNumber = Int.random(in: 1...100)
+    var body: some View {
+        VStack
+        {
+            List
+            {
+                Text("Apple")
+                    .badge(randomNumber)
+              
+            }
+        }
+    }
+}
+
+struct BadgeUI_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeUI()
+    }
+}
